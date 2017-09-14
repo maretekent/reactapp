b953973a94afff852e5442a842036a85026d7389
kenneth.marete <maretekent@gmail.com>
2017-09-14 15:53:41 +0300

    [WIP]

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+
+   	  var i = 1;
+
+      return (
+         <div>
+            <h1>Header</h1>
+            <h2>Content</h2>
+            <p>This is the content!!!</p>
+            <h1>{1+1}</h1>
+            <h1>{i == 1 ? 'True!' : 'False'}</h1>
+         </div>
+      );
+   }
+}
+
+export default App;
